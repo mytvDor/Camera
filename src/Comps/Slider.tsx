e6d60ac401534e173c9d67d88931e121c4306281
Suraj Ranajit Bhosale <surajranajitbhosale003@gmail.com>
2024-11-22 15:52:57 +0000

    added Slider Error Solve

--- src/Comps/Slider.tsx
+++ src/Comps/Slider.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../App.css"; // Optional: for custom styling
+import "../App.css"; 
 
 function ResponsiveSlider() {
   const images = [
