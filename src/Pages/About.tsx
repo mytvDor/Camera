e4e1713ae50157053b6e357b2ef859bad417d894
SurajBhosale003 <surajedu003@gmail.com>
2024-12-02 10:38:39 +0530

    added

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -127,13 +127,13 @@ export default function About() {
 <section className="py-5 relative">
     <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
         <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
-        <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover" 
+        <img className="lg:mx-0 mx-auto h-full rounded-3xl object-contain" 
                 src="https://res.cloudinary.com/de6u5kbiw/image/upload/v1732556964/swissco/content/c03656b8-3ae2-46f9-a1d9-43f7ff135f9f.png" alt="About Us image" />
            
         <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
                 <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
                     <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
-                        Our Mission
+                        Our Mission's
                     </h2>
                     <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
                         At SWISSCO Technology, we believe in harnessing diverse perspectives and strengths to foster environments 
