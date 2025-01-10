539db13eaa3a32d8f8e238b25b42ced1acfb3231
sumit <mytvdor@gmail.com>
2025-01-10 09:33:42 +0530

    pdf events

--- src/Pages/Events.tsx
+++ src/Pages/Events.tsx
@@ -105,6 +105,21 @@ OREGAON EAST, MUMBAI, INDIA`,
       ))}
       <Slider />
     </div>
+    <div className="flex flex-col items-center justify-center">
+  <iframe
+    title="Event 1 PDF"
+    className="m-2"
+    src="/Event1.pdf"
+    style={{ width: '100%', height: '100vh', maxWidth: '800px' }}
+  />
+  <iframe
+    title="Event 2 PDF"
+    className="m-2"
+    src="/Event2.pdf"
+    style={{ width: '100%', height: '100vh', maxWidth: '800px' }}
+  />
+</div>
+
     <div className="mx-auto bg-white-200 flex flex-col items-center justify-center px-8 py-8">
       <img src="https://res.cloudinary.com/de6u5kbiw/image/upload/v1733115267/swissco/events/f46502ad-c546-4895-b3ff-0222993c1de3.png" alt="" />
     </div>
